4934b1b8148a688de132fe19c1e206e8d84261c2
Ricardo Carvalho <ricorino1@gmail.com>
2021-05-21 20:17:16 -0300

    CT04

--- src/test/java/Teste.java
+++ src/test/java/Teste.java
@@ -22,38 +22,57 @@ public class Teste {
 		// total de livros cadastrados igual 1
 		assertEquals(1, biblioteca.size());
 	}
-	
+
 	@Test
-    public void ct02CadastrarComIsbnJaCadastrado() {
-        //dado que nao existem livros cadastrados
-        Biblioteca biblioteca = new Biblioteca();
-        //quando um livro não existe cadastro
-        Livro livro1 = new Livro();
-        livro1.setAutor("Thiago Leite e Carvalho");;
-        livro1.setIsbn("1000");
-        livro1.setTitulo("Orientação a Objetos");
-        biblioteca.save(livro1);
-        //e outro livro com o mesmo isbn não cadastrado
-        Livro livro2 = new Livro();
-        livro2.setAutor("Thiago Leite e Carvalho");;
-        livro2.setIsbn("1000");
-        livro2.setTitulo("Orientação a Objetos");
-        biblioteca.save(livro2);
-        //total de livros cadastrados deve ser igual 1
-        assertEquals(1, biblioteca.size());
-    }
+	public void ct02CadastrarComIsbnJaCadastrado() {
+		// dado que nao existem livros cadastrados
+		Biblioteca biblioteca = new Biblioteca();
+		// quando um livro não existe cadastro
+		Livro livro1 = new Livro();
+		livro1.setAutor("Thiago Leite e Carvalho");
+		;
+		livro1.setIsbn("1000");
+		livro1.setTitulo("Orientação a Objetos");
+		biblioteca.save(livro1);
+		// e outro livro com o mesmo isbn não cadastrado
+		Livro livro2 = new Livro();
+		livro2.setAutor("Thiago Leite e Carvalho");
+		;
+		livro2.setIsbn("1000");
+		livro2.setTitulo("Orientação a Objetos");
+		biblioteca.save(livro2);
+		// total de livros cadastrados deve ser igual 1
+		assertEquals(1, biblioteca.size());
+	}
+
 	@Test
-    public void ct03CadastrarAutorEmBranco() {
-        //dado que nao existem livros cadastrados
-        Biblioteca biblioteca = new Biblioteca();
-        //quando um livro  cadastrado com isbn em branco
-        Livro livro1 = new Livro();
-        livro1.setAutor("Thiago Leite e Carvalho");;
-        livro1.setIsbn("");
-        livro1.setTitulo("Orientação a Objetos");
-        biblioteca.save(livro1);
-        //total de livros cadastrados deve ser igual 0
-        assertEquals(0, biblioteca.size());
-    }
+	public void ct03CadastrarAutorEmBranco() {
+		// dado que nao existem livros cadastrados
+		Biblioteca biblioteca = new Biblioteca();
+		// quando um livro cadastrado com isbn em branco
+		Livro livro1 = new Livro();
+		livro1.setAutor("Thiago Leite e Carvalho");
+		;
+		livro1.setIsbn("");
+		livro1.setTitulo("Orientação a Objetos");
+		biblioteca.save(livro1);
+		// total de livros cadastrados deve ser igual 0
+		assertEquals(0, biblioteca.size());
+	}
+
+	@Test
+	public void ct04CadastrarTituloEmBranco() {
+		// dado que nao existem livros cadastrados
+		Biblioteca biblioteca = new Biblioteca();
+		// quando um livro não cadastrado com titulo em branco
+		Livro livro1 = new Livro();
+		livro1.setAutor("Thiago Leite e Carvalho");
+		;
+		livro1.setIsbn("1000");
+		livro1.setTitulo("");
+		biblioteca.save(livro1);
+		// total de livros cadastrados deve ser igual 0
+		assertEquals(0, biblioteca.size());
+	}
 
 }
